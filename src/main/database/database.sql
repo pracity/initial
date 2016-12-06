636b1f17ea8c947c20ed341a29918fff42c3422e
pracity <tathode@gmail.com>
2016-12-06 20:11:40 +0000

    database creation statement added user credentials added to application.properties

--- src/main/database/database.sql
+++ src/main/database/database.sql
@@ -1,3 +1,5 @@
- 
+ -- user = root , password=Passoword
+--create database pracity 
+-- use pracity
 
 create table product (p_id int not null AUTO_INCREMENT, p_code varchar(10), p_desc varchar(50), p_category int , primary key(p_id));
